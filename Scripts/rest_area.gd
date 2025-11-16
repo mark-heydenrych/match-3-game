b520f12c14c92d324b7ae21e49bf6e40b68fead4
Mark Heydenrych <mheydenrych@routeone.com>
2025-11-16 16:49:47 -0500

    dev-P95 Updated rest area logic

--- Scripts/rest_area.gd
+++ Scripts/rest_area.gd
@@ -1,8 +1,9 @@
 class_name RestArea
 extends Node2D
 
-var activated: bool = false
 var active: bool = true
+
+var recharge_cost = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,19 +17,29 @@ func _process(delta):
 
 func _on_recharge_button_pressed():
 	get_node("ClickPlayer").play()
-	if (!activated):
+	if (get_parent().spend_diamonds(recharge_cost)):
+		if (recharge_cost == 1):
+			recharge_cost = 2
+		else:
+			recharge_cost *= recharge_cost
 		print("Recharging")
 		get_parent().recharge()
-		activated = true
+		get_node("RechargeButton").text = str(recharge_cost) + " Shards: Recharge: +5 Turns"
+		get_node("DebugButton").text = str(recharge_cost) + " Shard: Debug (Remove Debuffs)"
 	pass # Replace with function body.
 
 
 func _on_debug_button_pressed():
 	get_node("ClickPlayer").play()
-	if (!activated):
+	if (get_parent().spend_diamonds(recharge_cost)):
+		if (recharge_cost == 1):
+			recharge_cost = 2
+		else:
+			recharge_cost *= recharge_cost
 		print("Debugging")
-		activated = true
 		get_parent().get_node("SideBoard").remove_all_debuffs()
+		get_node("RechargeButton").text = str(recharge_cost) + " Shards: Recharge: +5 Turns"
+		get_node("DebugButton").text = str(recharge_cost) + " Shard: Debug (Remove Debuffs)"
 	pass # Replace with function body.
 
 
